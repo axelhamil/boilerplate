8fe218a8137d26276cce15465d26770b4284eb95
Axel Hamilcaro <axel.hamilcaro@icloud.com>
2025-02-11 16:53:45 +0100

    initial commit

---
+++ apps/api/src/main.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+
+import { db, users } from "@packages/drizzle";
+import { fastify } from "@shared/fastify/config";
+
+fastify.get("/", async (_req, res) => {
+  const result = await db.query.users.findFirst();
+
+  if (!result) {
+    const [newUser] = await db
+      .insert(users)
+      .values({
+        email: "test@test.com",
+        name: "Test",
+      })
+      .returning();
+
+    return res.status(200).send(newUser);
+  }
+
+  res.status(200).send(result);
+});
+
+fastify.listen(
+  { port: Number(process.env.API_PORT), host: "localhost" },
+  (err) => {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+  },
+);
